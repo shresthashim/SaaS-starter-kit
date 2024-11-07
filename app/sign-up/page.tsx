fb070b4770caabd648ab13dac18b0fcebf8a58eb
AsHim Shrestha <ashimsth89@gmail.com>
2024-11-07 21:38:40 +0545

    feat: add clerk provider and setup shadcn ui

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <p>Sign up to create an account.</p>
+    </div>
+  );
+};
+export default SignUpPage;
